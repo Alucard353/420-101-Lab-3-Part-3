98d99bbfff4bf3237fdb964653da0953aa1cb242
noah <Noahm8042@gmail.com>
2025-09-12 22:47:12 -0400

    Question 48 and 49, adding the createAndPlay and the SimpleTune

--- TuneCreator.java
+++ TuneCreator.java
@@ -17,6 +17,29 @@ public class TuneCreator
     {
         player = new MidiPlayer();
     }
-
-
+    
+    /**
+     * Create a new tune and play it
+     */
+    public void createAndPlay()
+    {
+        SimpleTune tune = new SimpleTune();
+        
+        tune.addNote("G4", 2);
+        tune.addNote("G4", 2);
+        tune.addNote("G4", 4);
+        tune.addNote("G4", 2);
+        tune.addNote("A4", 6);
+        tune.addNote("E4", 4);
+        tune.addNote("G4", 4);
+        tune.addNote("G4", 4);
+        tune.addNote("C5", 2);
+        tune.addNote("D5", 6);
+        
+        tune.setInstrument(7);
+        
+        player.playTune(tune);
+    
     }
+    
+}
